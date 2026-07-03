4016440524b81be5aed5586eb9449971df982c1f
Kirill Chistov <kirill.v.chistov@yandex.ru>
2026-07-03 20:25:14 +0300

    Step 6: autotests and CI, fix filepath in tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -8,8 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 test('gendiff flat json', () => {
-  const filepath1 = path.join(__dirname, '__fixtures__', 'file1.json');
-  const filepath2 = path.join(__dirname, '__fixtures__', 'file2.json');
+  const filepath1 = path.join(__dirname, '..', 'data', 'file1.json');
+  const filepath2 = path.join(__dirname, '..', 'data', 'file2.json');
   const expected = fs.readFileSync(
     path.join(__dirname, '__fixtures__', 'expected.txt'),
     'utf-8',
